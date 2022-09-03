d1ec73d222a9b50985415902c32dcf723e81202e
thilina_dilshan <thilina.dilshan@arimaclanka.com>
2022-09-03 12:58:18 +0530

    serialize and deserialize

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -5,17 +5,37 @@ import { userCacheKeys } from '$services/keys';
 
 export const getUserByUsername = async (username: string) => {};
 
-export const getUserById = async (id: string) => {};
-
-export const createUser = async (attrs: CreateUserAttrs) => {
-	const { username, password } = attrs;
+export const getUserById = async (id: string) => {
 	try {
-		const userId = genId();
-		await client.hSet(userCacheKeys(userId), {
-			username,
-			password
-		});
+		const user = await client.hGetAll(userCacheKeys(id));
+		return deserialize(id, user);
 	} catch (error) {
 		console.error(error, 'something went wrong');
 	}
+};
+
+export const createUser = async (attrs: CreateUserAttrs): Promise<string> => {
+	const userId = genId();
+
+	try {
+		await client.hSet(userCacheKeys(userId), serialize(attrs));
+		return userId;
+	} catch (error) {
+		console.error(error, 'something went wrong');
+	}
+};
+
+const serialize = (user: CreateUserAttrs) => {
+	const { username, password } = user;
+	return { username, password };
+};
+
+const deserialize = (id: string, user: { [key: string]: string }) => {
+	const { username, password } = user;
+
+	return {
+		id,
+		username,
+		password
+	};
 };
