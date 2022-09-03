5dd79250ceecf6d5b81532548ced7ed2b883024d
thilina_dilshan <thilina.dilshan@arimaclanka.com>
2022-09-03 08:12:05 +0530

    create user and userKeys for the hSet

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,1 +1,3 @@
 export const pageCacheKeys = (id: string) => `pagecache#${id}`;
+
+export const userCacheKeys = (userId:string)=> `usercache#${userId}`
--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,8 +1,21 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
+import { client } from '$services/redis';
+import { userCacheKeys } from '$services/keys';
 
 export const getUserByUsername = async (username: string) => {};
 
 export const getUserById = async (id: string) => {};
 
-export const createUser = async (attrs: CreateUserAttrs) => {};
+export const createUser = async (attrs: CreateUserAttrs) => {
+	const { username, password } = attrs;
+	try {
+		const userId = genId();
+		await client.hSet(userCacheKeys(userId), {
+			username,
+			password
+		});
+	} catch (error) {
+		console.error(error, 'something went wrong');
+	}
+};
