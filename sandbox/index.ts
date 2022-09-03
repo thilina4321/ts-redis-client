a64fc38450cda5f3fcb4375907e47c2edc3f513e
thilina_dilshan <thilina.dilshan@arimaclanka.com>
2022-09-03 06:46:02 +0530

    hget all first commit

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,16 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+	// adding hSet
+	await client.hSet('car', {
+		color: 'green',
+		year: 1940
+	});
+
+	// fetching hGet
+	const car = await client.hGetAll('car');
+
+	console.log(car);
+};
 run();
