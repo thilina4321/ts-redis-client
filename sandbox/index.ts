2007ae8ebb13f091f592c91a73a8e66a1b732dd0
thilina_dilshan <thilina.dilshan@arimaclanka.com>
2022-09-03 06:47:04 +0530

    Hash gocha

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -5,11 +5,17 @@ const run = async () => {
 	// adding hSet
 	await client.hSet('car', {
 		color: 'green',
-		year: 1940
+		year: 1940 // owner : null => do not add null or underfined. It throw an error
+		// as redis try to call toString() when it try to store data
 	});
 
 	// fetching hGet
 	const car = await client.hGetAll('car');
+
+	// hGetAll provide empty object even it does not contain key
+	// so what we can do is like below
+
+	if (Object.keys(car).length === 0) return null;
 
 	console.log(car);
 };
